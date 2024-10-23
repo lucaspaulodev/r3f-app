66a2d5f84b355625e80e8f2f29a03ff05bcdcf5f
Lucas Paulo <lucaspaulodev@gmail.com>
2024-10-22 21:46:40 -0300

    Creating App component and installing r3f dependencies.

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export const App = () => {
+    return (
+        <div>
+            Soon to be a badass R3F application
+        </div>
+    )
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,7 @@
-import './style.css'
+import { App } from './App'
 import ReactDOM from 'react-dom/client'
+import './style.css'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
-root.render(
-    <>
-        Soon to be a badass R3F application
-    </>
-)
+root.render(<App/>)

c18554435adf6c3f4e06f3e43e42d370839d2839
Lucas Paulo <lucaspaulodev@gmail.com>
2024-10-23 04:53:59 -0300

    Adjusting camera.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,18 @@ import Scene from "./components/Scene"
 
 const App = () => {
     return (
-        <Canvas>
+        <Canvas
+            flat
+            gl={{
+                antialias: true,
+            }}
+            camera={{
+                fov: 45,
+                near: 0.1,
+                far: 200,
+                position: [3, 2, 6]
+            }}
+        >
             <Scene />
         </Canvas>
     )

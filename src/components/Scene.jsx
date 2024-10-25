c43203a34c14ab78af1e23bdc7667158bd6912e8
Lucas Paulo <lucaspaulodev@gmail.com>
2024-10-25 05:13:10 -0300

    Playing with drei helpers.

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,9 +1,10 @@
 import { useFrame } from "@react-three/fiber"
 import { useRef } from "react"
 import CustomGeometry from "./CustomGeometry"
-import { OrbitControls } from "@react-three/drei"
+import { Float, Html, MeshReflectorMaterial, OrbitControls, PivotControls, Text, TransformControls } from "@react-three/drei"
 
 const Scene = () => {
+    const sphereRef = useRef()
     const boxRef = useRef()
     const groupRef = useRef()
 
@@ -17,21 +18,51 @@ const Scene = () => {
             <directionalLight position={[1, 2, 3]} intensity={4.5} />
             <ambientLight intensity={1} />
             <group ref={groupRef}>
-                <mesh position-x={-2} scale={1}>
-                    <sphereGeometry />
-                    <meshStandardMaterial color="salmon" />
-                </mesh>
+                <PivotControls anchor={[0, 0, 0]}
+                    depthTest={false}
+                >
+                    <mesh ref={sphereRef} position-x={-2} scale={1}>
+                        <sphereGeometry />
+                        <meshStandardMaterial color="salmon" />
+                        <Html
+                            position={[1, 1, 0]}
+                            wrapperClass="label"
+                            center
+                            distanceFactor={6}
+                            occlude={[boxRef, sphereRef]}
+                        >
+                            That's a sphere 👍🏼
+                        </Html>
+                    </mesh>
+                </PivotControls>
+
+
                 <mesh ref={boxRef} position-x={2} scale={1.5}>
                     <boxGeometry args={[1, 1]} />
-                    <meshStandardMaterial color="purple" />
+                    <meshStandardMaterial color="mediumpurple" />
                 </mesh>
+                <TransformControls object={boxRef} />
+
             </group>
             <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
                 <planeGeometry />
-                <meshStandardMaterial color="green" />
+                <MeshReflectorMaterial color="greenyellow" resolution={512} blur={[1000,1000]} mixBlur={1} mirror={0.75}/>
             </mesh>
-            <OrbitControls/>
+            <OrbitControls makeDefault />
             {/* <CustomGeometry/> */}
+            <Float speed={5}
+                floatIntensity={2}
+            >
+                <Text
+                    fontSize={1.5}
+                    color="red"
+                    position-y={3}
+                >
+                    Lucas Paulo
+                </Text>
+            </Float>
+
+
         </>
 
     )

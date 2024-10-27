7f5a286a09b42c8bed2b0461d36a4e32decb5583
Lucas Paulo <lucaspaulodev@gmail.com>
2024-10-27 18:31:42 -0300

    Removing Pivot and Transform controls and adding color to the scene background.

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -8,45 +8,43 @@ const Scene = () => {
     const boxRef = useRef()
     const groupRef = useRef()
 
-    // useFrame((state, delta) => {
-    //     boxRef.current.rotation.y += delta
-    //     groupRef.current.rotation.y += delta
-    // })
+    useFrame((state, delta) => {
+        boxRef.current.rotation.y += delta
+        groupRef.current.rotation.y += delta
+    })
 
     return (
         <>
+            <color args={['ivory']} attach="background" />
             <directionalLight position={[1, 2, 3]} intensity={4.5} />
             <ambientLight intensity={1} />
             <group ref={groupRef}>
-                <PivotControls anchor={[0, 0, 0]}
-                    depthTest={false}
-                >
-                    <mesh ref={sphereRef} position-x={-2} scale={1}>
-                        <sphereGeometry />
-                        <meshStandardMaterial color="salmon" />
-                        <Html
-                            position={[1, 1, 0]}
-                            wrapperClass="label"
-                            center
-                            distanceFactor={6}
-                            occlude={[boxRef, sphereRef]}
-                        >
-                            That's a sphere 👍🏼
-                        </Html>
-                    </mesh>
-                </PivotControls>
+                <mesh ref={sphereRef} position-x={-2} scale={1}>
+                    <sphereGeometry />
+                    <meshStandardMaterial color="salmon" />
+                    <Html
+                        position={[1, 1, 0]}
+                        wrapperClass="label"
+                        center
+                        distanceFactor={6}
+                        occlude={[boxRef, sphereRef]}
+                    >
+                        That's a sphere 👍🏼
+                    </Html>
+                </mesh>
+
 
 
                 <mesh ref={boxRef} position-x={2} scale={1.5}>
                     <boxGeometry args={[1, 1]} />
                     <meshStandardMaterial color="mediumpurple" />
                 </mesh>
-                <TransformControls object={boxRef} />
+                {/* <TransformControls object={boxRef} /> */}
 
             </group>
             <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
                 <planeGeometry />
-                <MeshReflectorMaterial color="greenyellow" resolution={512} blur={[1000,1000]} mixBlur={1} mirror={0.75}/>
+                <meshStandardMaterial color="greenyellow" />
             </mesh>
             <OrbitControls makeDefault />
             {/* <CustomGeometry/> */}
